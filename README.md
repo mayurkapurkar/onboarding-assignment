f6ab169cc62dc03f169d9d7160c847c485e545a0
root <root@dev2099base1.int.coupadev.com>
2022-06-14 11:44:24 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# assignment1
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: assignment1
+# Recipe:: default
+#
+# Copyright (c) 2022 The Authors, All Rights Reserved.
