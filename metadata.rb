7c3906835b474977084f560c8558ebae14baae00
rundeck <rundeck@devrun559srv1.int.coupadev.com>
2022-06-16 10:56:31 +0000

    changes in metadata

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/assignment1/issues' if respond_to?(:issues_url)
-depends "assignment1"
+
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 #source_url 'https://github.com/<insert_org_here>/assignment1' if respond_to?(:source_url)
